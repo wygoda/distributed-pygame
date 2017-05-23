c27b123478713bb97a3b9f9bd8b54bb131cd3a65
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-05-23 18:31:25 +0200

    bugfix: bullets now spawn at the center of a player's rect

--- bullet.py
+++ bullet.py
@@ -5,21 +5,21 @@ from pygame.locals import *
 class Bullet:
 	def __init__(self, id, player):
 		self.id = id
-		self.rect = pygame.Rect(player.rect.centerx, player.rect.centery, 10, 10)
+		self.rect = pygame.Rect(player.rect.centerx - 5, player.rect.centery - 5, 10, 10)
 		self.angle = player.angle
 		self.speed = 10
-		
+
 		#components of bullet velocity vector
 		self.velocityX = -math.cos(player.angleRad)
 		self.velocityY = -math.sin(player.angleRad)
-		
+
 		#got to normalize velocity so the bullet won't be faster when fired diagonally
 		velocityMagnitude = math.sqrt(math.pow(self.velocityX,2)+math.pow(self.velocityY,2))
 		self.velocityX = self.velocityX*self.speed*velocityMagnitude
 		self.velocityY = self.velocityY*self.speed*velocityMagnitude
-		
+
 		self.image = pygame.image.load("bullet.png")
 		self.image = pygame.transform.rotate(self.image,90 - self.angle)
-		
+
 	def update(self):
 		self.rect.move_ip(self.velocityX,self.velocityY)#move rect in place by the velocity
