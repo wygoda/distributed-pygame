3907f133876466effe50c69af9b58e60ab62f1ff
mSzadko <marcin_szadkowski@outlook.com>
2017-05-30 18:21:35 +0200

    walls and sand is ready

--- maptest.py
+++ maptest.py
@@ -26,11 +26,43 @@ image = pygame.image.load("sprites/wall2.png")
 for col in range(1,WIDTH-1):
 	rect = pygame.Rect(col*TILESIZE,0,TILESIZE,TILESIZE)
 	windowSurface.blit(image,rect)
-	
+
 #draw upper right corner
 image = pygame.image.load("sprites/wall3.png")
-	
-	
+rect = pygame.Rect((WIDTH-1)*TILESIZE,0,TILESIZE,TILESIZE)
+windowSurface.blit(image,rect)
+
+#draw left wall sand and right wall
+
+for col in range(0,WIDTH):
+	for row in range(1,HEIGHT-1):
+		if col == 0:
+			image = pygame.image.load("sprites/wall4.png")
+			rect = pygame.Rect(col*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE)
+			windowSurface.blit(image,rect)
+		elif col == WIDTH-1:
+			image = pygame.image.load("sprites/wall5.png")
+			rect = pygame.Rect(col*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE)
+			windowSurface.blit(image,rect)
+		else:
+			image = pygame.image.load("sprites/sand.png")
+			rect = pygame.Rect(col*TILESIZE,row*TILESIZE,TILESIZE,TILESIZE)
+			windowSurface.blit(image,rect)
+#draw lower wall
+image = pygame.image.load("sprites/wall6.png")
+rect = pygame.Rect(0,(HEIGHT-1)*TILESIZE,TILESIZE,TILESIZE)
+windowSurface.blit(image,rect)
+
+
+image = pygame.image.load("sprites/wall7.png")
+for col in range(1,WIDTH-1):
+	rect = pygame.Rect(col*TILESIZE,(HEIGHT-1)*TILESIZE,TILESIZE,TILESIZE)
+	windowSurface.blit(image,rect)
+
+
+image = pygame.image.load("sprites/wall8.png")
+rect = pygame.Rect((WIDTH-1)*TILESIZE,(HEIGHT-1)*TILESIZE,TILESIZE,TILESIZE)
+windowSurface.blit(image,rect)
 	
 # Draw the window onto the screen.
 pygame.display.update()
